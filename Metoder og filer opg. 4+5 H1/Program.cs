5b8fe53fadd48964d9fd57410c7de76bc32099f8
Sillebob <78139966+Sillebob@users.noreply.github.com>
2021-01-29 20:07:56 +0100

    Add project files.

---
+++ Metoder og filer opg. 4+5 H1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Metoder_og_filer_opg._4_5_H1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opgave 4 Opret en mappe:
+
+            Directory.CreateDirectory(@".\Droids");
+
+            //Opgave 5: Slet en mappe rekursivt
+
+            File.WriteAllText(@".\Droids\R2D2.txt", "beep bop"); //opretter først en fil med tekst så der er noget at slette rekursivt
+            Directory.Delete(@".\Droids", true);
+
+        }
+    }
+}
